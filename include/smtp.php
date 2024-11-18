4c6a7d79f5fe6af05ce695fb50570bd629e0a5ca
xmartina <martinsprince126@gmail.com>
2024-11-18 13:09:59 +0100

    d

---
+++ include/smtp.php
@@ -0,0 +1,36 @@
+<?php
+const rootDir = '/home/multistream6/domains/dashboard.zyntrab.online/public_html/';
+require rootDir . 'include/vendor/autoload.php';
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+// MESSAGE & EMAIL CONFIGURATION FOR SCRIPT
+class message{
+    private $conn;
+    public function send_mail($email, $message, $subject){
+
+        $mail = new PHPMailer();
+        //SMTP Settings
+        //$mail->isSMTP();
+        $mail->isMail();
+        $mail->Host = "mail.zyntrab.online"; // Change Email Host
+        $mail->SMTPAuth = true;
+        $mail->Username = "contact@zyntrab.online"; // Change Email Address
+        $mail->Password = '@SECzyntrab1'; // Change Email Password
+        $mail->Port = 587; //587
+        $mail->SMTPSecure = "ssl"; //tls
+
+        //Email Settings
+        $mail->isHTML(true);
+        $mail->setFrom('contact@zyntrab.online',' Zyntra Bank'); // Change
+        $mail->addAddress($email);
+        $mail->AddReplyTo("contact@zyntrab.online", " Zyntra Bank"); // Change
+        $mail->Subject = $subject;
+        $mail->MsgHTML($message);
+        $mail->Send();
+
+
+    }
+
+}
